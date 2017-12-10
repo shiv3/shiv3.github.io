1f8c1b64680e6521240cbf661ccae22818692b6d
avis <b3326@oic.jp>
2017-12-10 20:51:22 +0900

    add

--- rakugaki/rc4/rc4.js
+++ rakugaki/rc4/rc4.js
@@ -13,12 +13,12 @@ function* PRGA(S){
         i = (i + 1) % 256;
         j = (j + S[i]) % 256;
         [S[i], S[j]] = [S[j], S[i]];
-        let Z =  S[(S[i] + S[j]) % 256];   
+        let Z =  S[(S[i] + S[j]) % 256];
         yield Z;
     }
 }
 const S2B = (src) => { //String2Byte
-  let bytes = [];  
+  let bytes = [];
   Array.prototype.forEach.call(src, (s) => {
     bytes.push(s.charCodeAt());
   });
@@ -36,7 +36,7 @@ const RC4 = (data,key) => {
     let result = [];
     d.forEach((c) => {
         let n = gen.next().value;
-        let r=String.fromCharCode( c^n );
+          let r=String.fromCharCode( c^n );
         let t=(c^n).toString(16)
         result.push(r);
     });
@@ -53,4 +53,3 @@ const ciphertext = RC4(message, key);
 console.log("暗号文:" + ciphertext);
 const message2 = RC4(ciphertext, key);
 console.log("復号化:" + message2);
-
